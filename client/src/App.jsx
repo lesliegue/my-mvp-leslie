5295fd35317beda0141d5395c8573398e4520e98
Helen-Porter <iamhelenporter@gmail.com>
2024-08-05 03:37:31 -0500

    Week layout rendered, Not on separate component yet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [allGoals, setallGoals] = useState([]);
+  
+
+  useEffect(() => {
+    fetch("/api/goals")
+    .then(res => res.json())
+    .then(response => {
+      setallGoals(response)
+      console.log("Goals Fetched")
+    })
+    .catch(error => {
+      console.log(error)
+    });
+  }, []);
 
   return (
     <>
+    <div className='week-layout'>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>MONDAY</h2>
+          <ul>
+            {allGoals.map((e, i)=> 
+              ((e.dayofweek === "monday") && (
+              <li key = {e.id}>{e.goal}</li>
+              )
+            ))}
+          </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div>
+        <h2>TUESDAY</h2>
+          <ul>
+            {allGoals.map((e, i)=> 
+              ((e.dayofweek === "tuesday") && (
+              <li key = {e.id}>{e.goal}</li>
+              )
+            ))}
+          </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div>
+        <h2>WEDNESDAY</h2>
+          <ul>
+            {allGoals.map((e, i)=> 
+              ((e.dayofweek === "wednesday") && (
+              <li key = {e.id}>{e.goal}</li>
+              )
+            ))}
+          </ul>
+      </div>
+      
+      <div>
+        <h2>THURSDAY</h2>
+          <ul>
+            {allGoals.map((e, i)=> 
+              ((e.dayofweek === "thursday") && (
+              <li key = {e.id}>{e.goal}</li>
+              )
+            ))}
+          </ul>
+      </div>
+
+      <div>
+        <h2>FRIDAY</h2>
+          <ul>
+            {allGoals.map((e, i)=> 
+              ((e.dayofweek === "friday") && (
+              <li key = {e.id}>{e.goal}</li>
+              )
+            ))}
+          </ul>
+      </div>
+
+      <div>
+        <h2>SATURDAY</h2>
+          <ul>
+            {allGoals.map((e, i)=> 
+              ((e.dayofweek === "saturday") && (
+              <li key = {e.id}>{e.goal}</li>
+              )
+            ))}
+          </ul>
+      </div>
+
+      <div>
+        <h2>SUNDAY</h2>
+          <ul>
+            {allGoals.map((e, i)=> 
+              ((e.dayofweek === "sunday") && (
+              <li key = {e.id}>{e.goal}</li>
+              )
+            ))}
+          </ul>
+      </div>
+    </div>
+
     </>
   )
 }
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,15 @@ router.get('/', function(req, res, next) {
   res.send('heres your message');
 });
 
+router.get('/goals', async (req, res) => {
+  try {
+    let results = await db(`SELECT * FROM goals;`);
+  res.send (results.data)
+  } catch (err) {
+    res.status(500).send({error: err.message});
+  }
+});
+
 
 router.post('/goals', async (req, res) =>{
   let { goal,
@@ -37,16 +46,47 @@ router.post('/rewards', async (req, res) => {
   }
 });
 
+
+
 router.put('/goals', async (req, res) => {
- let { id } = req.body;
-  try {
-    await db(`UPDATE goals SET completed = true WHERE id = ${id};`);
-    const results = await db(`SELECT * FROM goals;`);
-    res.send(results.data);
-  } catch (err) {
-    res.status(500).send({error: err.message});
-  }
-});
+  let { id } = req.body;
+   try {
+     await db(`UPDATE goals SET completed = !completed WHERE id = ${id};`);
+     let score = await db(`SELECT SUM(difflevel) AS total FROM goals WHERE completed = true;`)
+      res.send(score.data[0].total)
+    } catch (err) {
+      res.status(500).send({error: err.message});
+    }
+  });
+
+// router.put('/goals', async (req, res) => {
+//  let { id } = req.body;
+//   try {
+//     await db(`UPDATE goals SET completed = !completed WHERE id = ${id};`);
+//     const results = await db(`SELECT * FROM goals;`);
+    // res.send(results.data);
+
+    // const getLevel = (score) => {
+
+    //   console.log('inside getLevel score.data', score)
+    //   if (+score < 10) {
+    //     return "Silver";
+    //   } else if (+score < 20) {
+    //     return "Gold";
+    //   } else {
+    //     return "Diamond";
+    //   }
+    // }
+
+
+//     let score = await db(`SELECT SUM(difflevel) AS total FROM goals WHERE completed = true;`)
+//    const total = score[0].total;
+//     // const result = getLevel(score.data[0])
+//     res.send({ total })
+//   } catch (err) {
+//     res.status(500).send({error: err.message});
+//   }
+// });
 
 router.get('/rewards', async (req, res) => {
   try {
